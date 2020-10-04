e17f8c6acf09f0e4dcfc01db03458f0f2dab92ee
repositorio-estudo <rodrigo.carrilho.soa@gmail.com>
2020-10-04 18:04:54 -0300

    Primeiro commit para teste

---
+++ src/main/java/com/coimbra/api/NovoTesteProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.coimbra.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoTesteProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoTesteProjetoApplication.class, args);
+	}
+
+}
